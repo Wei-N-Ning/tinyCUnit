8ba58104a61293de75bc27fb7b958429ae34f497
Wei Ning <nwcgnw@gmail.com>
2018-03-11 00:49:11 +1300

    - add cmake generator to create auto test runner

---
+++ test_nomain.c
@@ -0,0 +1,19 @@
+//
+// Created by wein on 3/10/18.
+//
+
+void RunTinyTests();
+
+void test_given_when_then() {
+    int i = 0;
+    i += 10;
+}
+
+void test_arrange_action_assert() {
+    int i = 1;
+    i -= 10;
+}
+
+int main(int argc, char **argv) {
+    RunTinyTests();
+}
